6f78c8ebe5265ef0bd2cdfa4e95724cdadcf49ef
Matthew Dean <414752+matthew-dean@users.noreply.github.com>
2026-09-22 19:05:08 -0700

    Version 1.0.0-alpha.3

--- packages/html-next/src/generate.ts
+++ packages/html-next/src/generate.ts
@@ -6,7 +6,7 @@ import { HtmlDiagnosticError } from "./diagnostics.js";
 import type { ComponentDefinition } from "./template.js";
 import { compileComponentStylesForBuild } from "./component-styles-build.js";
 
-export const GENERATOR_VERSION = "1.0.0-alpha.2";
+export const GENERATOR_VERSION = "1.0.0-alpha.3";
 
 export interface GeneratedArtifact {
   readonly path: string;
